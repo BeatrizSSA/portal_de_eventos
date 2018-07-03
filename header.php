8cbc81b05391ac220b7cbba13c0978056c662360
skylria <jullyannalovely@gmail.com>
2018-07-03 07:51:06 -0300

    criação das páginas de admin e evento

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.css">
+	<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+  <script src="semantic/dist/semantic.js"></script>
+  <title>Página administrativa</title>
+</head>
+<body>
+  <nav>
+   <div class="ui text menu">
+    <div class="header item active">
+      <img src="semantic/img/ifpe.png" alt="Logo" height="100" width="100"></div>
+      <div class="right menu">
+        <a class="item ui">
+          Meus Eventos
+        </a>
+        <a class="ui item">
+          Ajuda
+        </a>
+        <div class="item">
+          <div class="ui button"><i class="user icon"></i>Admin</div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+include "header.php";
+?>
+
+<div class="ui three cards">
+    <div class="blue card">
+      <div class="content">
+       <button class="huge ui fluid center aligned container segment">
+        <a href="newEvent.php">
+         Novo Evento
+         <i class="huge link plus icon"></i>
+       </a>
+     </button>
+   </div>		
+ </div>
+ <div class="grey card">
+  <div class="image">
+   <img src="semantic/img/entec.png">
+ </div>
+ <div class="content">
+   <h2 class=" header center aligned">Entec 2017</h2>
+   <div class=" meta description">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua...
+  </div>
+  <div class="ui divider"></div>
+  <div class="extra content">
+    <button class="fluid ui button">
+      <i class="large edit icon"></i>
+      Editar Evento
+    </button>
+  </div>
+</div>
+</div>
+<div class="black card">
+  <div class="image">
+   <img src="semantic/img/logmaster.png">
+ </div>
+ <div class="content">
+   <h2 class=" header center aligned">LogMaster</h2>
+   <div class=" meta description">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua...
+  </div>
+  <div class="ui divider"></div>
+  <div class="extra content">
+    <button class="fluid ui button">
+      <i class="large edit icon"></i>
+      Editar Evento
+    </button>
+  </div>
+</div>
+</div>
+</div>
+</body>
+</html>
